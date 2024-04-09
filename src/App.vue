15a002a1dd97288e8a3b942349366df710120f6a
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-09 16:33:15 +0200

    appTitle

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <AppTitleComponent/>
+</template>
+
+<script>
+  import AppTitleComponent from './components/AppTitleComponent.vue',
+  import AppHeroComponent from './components/AppHeroComponent.vue'
+  export default {
+    name: 'App',
+    components: {
+      AppTitleComponent
+    },
+    data(){
+      return {
+        
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitleComponent',
+        data(){
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
